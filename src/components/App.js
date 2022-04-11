d64ccf919f27e75cb1024ced2a6958d26303917e
Animesh Srivastava <85071104+Aman-Animesh@users.noreply.github.com>
2022-04-11 18:21:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h2 data-ns-test="project-name">Bloody</h2>
+      <p data-ns-test="project-name">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      </p>
+    </div>
   )
 }
 
